b0ae32158466d2009e24e87fe5c1f95a3c031995
Joshua Diaz <jad24096@gmail.com>
2022-05-08 23:30:43 -0400

    added new post routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const { route } = require("../../../.Main/controllers/api");
 
 const router = require("express").Router();
-
+const userRoutes = require("");
+const postRoutes = require("");
+const commentRoutes = require("");
 // Finish the required pathing for these variables
 // const userRoutes = ;
 // const postRoutes = ;
@@ -9,5 +11,7 @@ const router = require("express").Router();
 
 router.use("/user", userRoutes);
 // write the rest of the router.use routes
+router.use("/", postRoutes);
+router.use("/", commentRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,10 +3,33 @@ const { Post, Comment, User } = require("../models");
 
 router.get("/", async (req, res) => {
     // get all posts for the homepage
+    try {
+        const homeData = await HomePage.findAll({
+          include: [ Category, 
+            { model: Tag, through: ProductTag }],
+        });
+        res.status(200).json(homeData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 router.get("/post/:id", async (req, res) => {
     // get a single post
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+          include: [ Category, 
+            { model: Tag, through: ProductTag }],
+        });
+        if (!postData) {
+          res.status(404).json({ message: 'No post found with that id!'});
+          return;
+        }
+    
+        res.status(200).json(postData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 router.get("/login", (req, res) => {
