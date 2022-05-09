587558255e07319d58744c096431e5dc748abd67
Joshua Diaz <jad24096@gmail.com>
2022-05-08 22:57:29 -0400

    added routes that need to be edited.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,14 +3,32 @@ const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
     // Creating a new instance of user
+   try {
+       const userData = await User.create(req.body);
+       res.status(200).json(userData);
+   } catch (err) {
+       res.status(400).json(err);
+   }
 });
 
 router.post("/login", async (req, res) => {
     // User login
+    try {
+        const loginData = await Login.create(req.body);
+        res.status(200).json(loginData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.post("/logout", async (req, res) => {
     // User logout
+    try {
+        const logoutData = await Logout.create(req.body);
+        res.status(200).json(logoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,14 +4,50 @@ const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
     // we want to go ahead and finishing the routing to get all the posts
+    try {
+        const dashboardData = await Dashboard.findAll({ 
+            include: []
+      });
+      res.status(200).json(dashboardData);
+    } catch (err) {
+      res.status(500).json(err);
+      }
 });
 
 router.get("/new", withAuth, (req, res) => {
 // for showing new posts to the user
+    try {
+        const newData = await New.findByPk(req.params.id, {
+        include: [ Product ]
+        });
+
+        if (!newData) {
+        res.status(404).json({ message: 'No user found with this id'});
+        return;
+        }
+
+        res.status(200).json(newData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 router.get("/edit/:id", withAuth, async (res, req) => {
     // To be able to find posts by primary key and render the edit post on the dashboard
+    try {
+        const postData = await Category.findByPk(req.params.id, {
+          include: [ Product ]
+        });
+    
+        if (!postData) {
+          res.status(404).json({ message: 'No category found with this id'});
+          return;
+        }
+    
+        res.status(200).json(postData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 })
 
 module.exports = router;
